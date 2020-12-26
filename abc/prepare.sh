9e753195653b63067ec31a7c3f61c6883e62bfd8
fjnkt98 <50233866+fjnkt98@users.noreply.github.com>
2020-12-26 20:21:03 +0900

    Add: 準備用スクリプトファイル

---
+++ abc/prepare.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+TARGET=$1
+
+mkdir $TARGET
+
+cp main.template $TARGET/a.cpp
+cp main.template $TARGET/b.cpp
+cp main.template $TARGET/c.cpp
+cp main.template $TARGET/d.cpp
+cp main.template $TARGET/e.cpp
+cp main.template $TARGET/f.cpp
+
+cp Makefile.template $TARGET/Makefile
