9b1abb8ca3c8955d79850e42816470f4a9b3cf15
fjnkt98 <50233866+fjnkt98@users.noreply.github.com>
2021-07-19 21:46:50 +0900

    Update: Correct answer

--- contests/abc/210/d.cpp
+++ contests/abc/210/d.cpp
@@ -4,6 +4,9 @@
 #include <algorithm>
 #include <utility>
 #include <cmath>
+#include <cstdint>
+
+const int64_t INF = 1LL << 60;
 
 int main() {
   int64_t H, W, C;
@@ -15,39 +18,55 @@ int main() {
     }
   }
 
-  std::pair<int64_t, int64_t> a(0, 0);
-  std::pair<int64_t, int64_t> b(0, 1);
-  int64_t min_cost = A.at(0).at(0) + A.at(0).at(1) + C;
+  // DP table for left to right search
+  std::vector<std::vector<int64_t>> dp(H + 1, std::vector<int64_t>(W + 1, INF));
 
+  int64_t minimum_cost = INF;
+  // DP start(left to right search)
   for (int64_t i = 0; i < H; i++) {
     for (int64_t j = 0; j < W; j++) {
-      if (i == 0 && j == 0) {
-        continue;
-      }
-      if (i == 0 && j == 1) {
-        continue;
-      }
-
-      int64_t cost_a = A.at(i).at(j) + A.at(a.first).at(a.second)
-                      + C * (std::abs(i - a.first) + std::abs(j - a.second));
-      int64_t cost_b = A.at(i).at(j) + A.at(b.first).at(b.second)
-                      + C * (std::abs(i - b.first) + std::abs(j - b.second));
-
-      if (cost_a < min_cost || cost_b < min_cost) {
-        if (cost_a < cost_b) {
-          b.first = i;
-          b.second = j;
-          min_cost = cost_a;
-        } else {
-          a.first = i;
-          a.second = j;
-          min_cost = cost_b;
-        }
+      dp.at(i + 1).at(j + 1) = std::min(
+          A.at(i).at(j),
+          std::min(
+            dp.at(i).at(j + 1) + C,
+            dp.at(i + 1).at(j) + C
+            )
+          );
+      int64_t X = std::min(
+          dp.at(i).at(j + 1) + C + A.at(i).at(j),
+          dp.at(i + 1).at(j) + C + A.at(i).at(j)
+          );
+      if (X < minimum_cost) {
+        minimum_cost = X;
       }
     }
   }
 
-  std::cout << min_cost << std::endl;
+  // DP table for right to left search
+  for (int64_t i = 0; i < H + 1; i++) {
+    dp.at(i).assign(W + 1, INF);
+  }
+  // DP start(right to left search)
+  for (int64_t i = 0; i < H; i++) {
+    for (int64_t j = W - 1; j >= 0; j--) {
+      dp.at(i + 1).at(j) = std::min(
+          A.at(i).at(j),
+          std::min(
+            dp.at(i).at(j) + C,
+            dp.at(i + 1).at(j + 1) + C
+            )
+          );
+      int64_t X = std::min(
+          dp.at(i).at(j) + C + A.at(i).at(j),
+          dp.at(i + 1).at(j + 1) + C + A.at(i).at(j)
+          );
+      if (X < minimum_cost) {
+        minimum_cost = X;
+      }
+    }
+  }
+
+  std::cout << minimum_cost << std::endl;
 
   return 0;
 }
