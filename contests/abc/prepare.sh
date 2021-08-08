a2b441f5e09a97cedec32a3dac05dc2554d09277
fjnkt98 <50233866+fjnkt98@users.noreply.github.com>
2021-08-08 20:43:32 +0900

    Update: adapt to changes number of problem

--- contests/abc/prepare.sh
+++ contests/abc/prepare.sh
@@ -11,5 +11,7 @@ cp main.template $TARGET/c.cpp
 cp main.template $TARGET/d.cpp
 cp main.template $TARGET/e.cpp
 cp main.template $TARGET/f.cpp
+cp main.template $TARGET/g.cpp
+cp main.template $TARGET/h.cpp
 
 cp Makefile.template $TARGET/Makefile
